1f49c63ac2d404ef001973a0c26ab370a00a98ae
Christian Doeller <mail@christiandoeller.de>
2024-06-30 16:53:49 +0200

    multer test postman ok

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -1,23 +1,37 @@
-const fileUploader = require("../config/cloudinary.config")
-const router = require("express").Router()
+const fileUploader = require("../config/cloudinary.config");
+const router = require("express").Router();
 
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
-router.post("/", fileUploader.array("images", 5), isAuthenticated, (req, res, next) => {
+// body form key must be called "file"
+router.post(
+    "/single",
+    fileUploader.single("file"),
+    isAuthenticated,
+    (req, res, next) => {
+      if (!req.file) {
+        next(new Error("No File Uploaded!"));
+        return;
+      }
+      res.json({ fileUrl: req.file.path });
+    }
+  );
+
+// body form key muste be called "files"
+router.post(
+  "/multiple",
+  fileUploader.array("files", 5),
+  isAuthenticated,
+  (req, res, next) => {
     if (!req.files || req.files.length === 0) {
       next(new Error("No files uploaded!"));
       return;
     }
-    const fileUrls = req.files.map(file => file.path);
+    const fileUrls = req.files.map((file) => file.path);
     res.json({ fileUrls });
-  });
+  }
+);
 
-router.post("/", fileUploader.single("imageUrl"), isAuthenticated, (req,res,next)=>{
-    if(!req.file){
-        next(new Error("No File Uploaded!"))
-        return
-    }
-    res.json({fileUrl:req.file.path})
-})
 
-module.exports = router
+
+module.exports = router;
