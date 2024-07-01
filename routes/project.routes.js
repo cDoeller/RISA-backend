c335a5ee88f36bdce27a2541555af0593f428130
Christian Doeller <mail@christiandoeller.de>
2024-07-01 11:30:17 +0200

    added populate projects route

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -8,6 +8,8 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 // GET :: all projects
 router.get("/", (req, res) => {
   Project.find()
+    .populate("contributors")
+    .populate("research_project")
     .then((Projects) => {
       res.status(200).json(Projects);
     })
